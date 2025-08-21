fdaf2406be1da88f33cc6d9536da142d467365d2
Amit Verma <amitkverma551@gmail.com>
2025-08-21 11:22:08 +0530

    navbar responsivenss

--- src/Components/layout/header/index.jsx
+++ src/Components/layout/header/index.jsx
@@ -1,6 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import Dropdown from "../../elements/dropdown";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import { FaBars, FaTimes } from "react-icons/fa"; // Icons
 
 const navLinks = [
   {
@@ -9,8 +13,8 @@ const navLinks = [
     children: [
       { path: "it-staffing-solutions", title: "IT Staffing Solution" },
       { path: "payroll-and-operations", title: "Payroll and Operation" },
-        { path: "/staffing/opt2", title: "Contract Staffing Solution" },
-      { path: "/staffing/opt2", title: "Complinace Services" },
+      { path: "/staffing/opt2", title: "Contract Staffing Solution" },
+      { path: "/staffing/opt2", title: "Compliance Services" },
     ],
   },
   {
@@ -21,14 +25,8 @@ const navLinks = [
       { path: "/elibrary/opt2", title: "e-Library Option 2" },
     ],
   },
-  {
-    title: "About Us",
-    path: "/about-us", // Simple link
-  },
-  {
-    title: "Talk To Us",
-    path: "/talkto-us", // Simple link
-  },
+  { title: "About Us", path: "/about-us" },
+  { title: "Talk To Us", path: "/talkto-us" },
   {
     title: "Job Opportunity",
     path: "/job-opportunity",
@@ -40,30 +38,61 @@ const navLinks = [
   },
 ];
 
-const Navbar = () => {
+const Header = () => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
-    <div className="header-container">
-      <p>Logo</p>
-      <ul className="nav-item-container">
-        {navLinks.map((link, idx) =>
-          link.children && link.children.length > 0 ? (
-            <Dropdown
-              key={`${idx}-${link.title}`}
-              label={link.title}
-              path={link.path}
-              items={link.children}
-            />
-          ) : (
-            <li key={`${idx}-${link.title}`} className="nav-item">
-              <NavLink to={link.path} className="nav-link">
-                {link.title}
-              </NavLink>
-            </li>
-          )
-        )}
+    <nav className="navbar navbar-expand-lg ">
+  <div className="container-fluid">
+    <a className="navbar-brand text-white" href="">Trinetra</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav m-auto mb-2 mb-lg-0 gap-4  ">
+        <li className="nav-item ">
+          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" href="#">Staffing</a>
+           <ul className="dropdown-menu">
+            <li>
+              <NavLink className="dropdown-item" to="it-staffing-solutions"> IT Staffing Solution</NavLink>
+              </li>
+              <li>
+              <NavLink className="dropdown-item" to="payroll-and-operations">Payroll and Operation</NavLink>
+              </li>
+         
+         
+          
+          </ul>
+          <ul>
+
+          </ul>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            e-Library
+          </a>
+          <ul className="dropdown-menu">
+            <li><NavLink className="dropdown-item" to="">Action</NavLink></li>
+            <li><NavLink className="dropdown-item" to="">Another action</NavLink></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><NavLink className="dropdown-item" to="">Something else here</NavLink></li>
+          </ul>
+        </li>
+        <li className="nav-item">
+          <NavLink className="nav-link " to="about-us">About us</NavLink> 
+        </li>
+        <li className="nav-item">
+        <NavLink className="nav-link" to="talkto-us">Talk to us</NavLink>
+        </li>
       </ul>
+      
     </div>
+  </div>
+</nav>
   );
 };
 
-export default Navbar;
+export default Header;
